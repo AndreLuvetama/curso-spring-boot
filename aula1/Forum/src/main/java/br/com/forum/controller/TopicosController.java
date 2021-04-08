833c09f10183df5019661a2f78bf85286afb2649
AndreLuvetama <mbiyavanga.luvetama@oi.net.br>
2021-04-08 17:39:09 -0300

    Inserindo repository

--- aula1/Forum/src/main/java/br/com/forum/controller/TopicosController.java
+++ aula1/Forum/src/main/java/br/com/forum/controller/TopicosController.java
@@ -1,24 +1,28 @@
 package br.com.forum.controller;
 
-import java.util.Arrays;
+
 import java.util.List;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.forum.dto.TopicoDto;
-import br.com.forum.modelo.Curso;
+
 import br.com.forum.modelo.Topico;
+import br.com.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 	
+	@Autowired
+	private TopicoRepository topicoRepository; 
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista(){
-		Topico topico = new Topico("Duvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+		List<Topico> topicos =  topicoRepository.findAll();
 		
-		return TopicoDto.convert(Arrays.asList(topico,topico,topico));
+		return TopicoDto.convert(topicos);
 	}
 
 }
